fc4ed682eafb8061e14dea194c0a0cde1a952ae8
jdu211171 <211171m@jdu.uz>
2023-02-09 08:03:45 +0500

    some minor changes

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
 import product from "../database/product.json" assert {type: "json"}
 
+for (let i = 0; i < product.length; i++) {
+    let content = document.querySelector('.content');
+    let newCard =   `<div class="card">
+                        <div class="up">
+                            <img src="./media/${product[i].img}" alt="" srcset="">
+                        </div>
+                        <div class="down">
+                            <input type="button" value="About" class="button-left">
+                            <input type="button" value="Buy" class="button-right">
+                        </div>
+                    </div>`;
+                    console.log(newCard);
+    content.innerHTML += newCard;
+}
