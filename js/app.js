ea4722a8818687e994c1f7b8f4ac8ebeab6b1512
jdu211171 <211171m@jdu.uz>
2023-02-13 14:34:00 +0500

    buy button functionality added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 import product from "../database/product.json" assert {type: "json"}
 
+
 let j = 1;
 for (let i = 0; i < product.length; i++) {
     let content = document.querySelector('.content');
@@ -9,16 +10,60 @@ for (let i = 0; i < product.length; i++) {
                         </div>
                         <div class="down">
                             <input type="button" value="About" class="button-left">
-                            <input type="button" value="Buy" class="button-right" productId="${j}">
+                            <input type="button" value="Buy" class="button-right" data-product-id="${j}">
                         </div>
                     </div>`;
     content.innerHTML += newCard;
     j++;
 }
 
+
+
+const magazine = document.querySelector('.right-navbar');
+
+document.querySelectorAll('.button-right').forEach(item => {
+    item.addEventListener('click', addToCard)
+});
+
+function addToCard(event) {
+    let id = event.target.dataset.productId;
+    for(let i = 0; i < product.length; i++){
+        if (id == product[i].id){
+            let card = document.createElement('div');
+            card.innerHTML += `<div class="card">
+                        <div class="up">
+                            <img src="./media/${product[i].img}" alt="" srcset="">
+                        </div>
+                        <div class="down">
+                            <input type="button" value="Remove" class="remove">
+                        </div>
+                    </div>`;
+            magazine.append(card);
+
+            let remove = document.querySelector('.remove');
+            remove = remove[remove.length - 1]
+            remove.addEventListener('click', (e) => {
+                console.log(e);
+                e.target.parentNode.parentNode.remove();
+            });
+            break;
+        }
+    }
+}
+
+/*
 let btnBuy = document.querySelectorAll('.button-right');
 for (let i of btnBuy) {
     i.addEventListener('click', function () {
-        console.log(this)
+        let sidebar = document.querySelector('.right-navbar');
     });
 }
+*/
+
+/*
+localStorage.setItem(product, [1, 2, 4, 5]);
+console.log(product[0])
+function addToCard(id) {
+    
+}
+*/
